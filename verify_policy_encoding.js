ad3cb8553bf4ab2e393479a32dd3cda15807d8eb
Wentao Guo <oatnewguo@gmail.com>
2019-01-22 15:18:15 -0800

    Updated rules for policy encoding.

--- verify_policy_encoding.js
+++ verify_policy_encoding.js
@@ -1,18 +1,26 @@
 /*
 RULES
 
-1.  Each node must have properties id, edges_out, layer, identifiable, label, and text.
+1.  Each node must have properties id, successors, layer, identifiable, label, and text.
 2.  Each node must have a non-empty string value for id.
 3.  Each node must have a unique id.
-4.  Each node must have a string array value for edges_out, where all array elements are ids of other nodes.
-5.  No node can be in the edges_out array of more than one other node (i.e., each node can have at most one parent).
-6.  Each node must have 0, 1, 2, or 3 as the value for layer.
-7.  Exactly one node must have 0 as the value for layer.
-8.  Each node must have a boolean value for identifiable.
-9.  Each node must have a non-empty string value for label.
-10. Each node must have a string value for text (it can be an empty string).
+4.  Each node must have a string array value for successors, where all array elements are ids of other nodes.
+5.  No node can be in the successors array of more than one other node (i.e., each node can have at most one parent).
+6.  Each node must have a value for layer less than or equal to those of its successors (i.e., links cannot travel inwards).
+!! UNCHECKED!! 7.  Each node must be connected by edges to the node at layer 0.
+8.  Each node must have 0, 1, 2, or 3 as the value for layer.
+9.  Exactly one node must have 0 as the value for layer.
+10. Each node must have a boolean value for identifiable.
+11. Each node must have a non-empty string value for label.
+12. Each node must have a string value for text (it can be an empty string).
 
 */
+
+// return the node with a given id from a list of nodes
+function getNodeById(node_id, nodes_data)
+{
+  return nodes_data.find(function(node) { return node.id == node_id; });
+}
 
 // check that a policy encoding does not violate the rules listed above
 function verifyPolicyEncoding(nodes_data)
@@ -28,8 +36,8 @@ function verifyPolicyEncoding(nodes_data)
       n = nodes_data[i];
 
       // Rule 1
-      if(! (n.hasOwnProperty("id") && n.hasOwnProperty("edges_out") && n.hasOwnProperty("layer") && n.hasOwnProperty("identifiable") && n.hasOwnProperty("label") && n.hasOwnProperty("text")))
-        throw "Not every node has properties id, edges_out, layer, identifiable, label, and text.";
+      if(! (n.hasOwnProperty("id") && n.hasOwnProperty("successors") && n.hasOwnProperty("layer") && n.hasOwnProperty("identifiable") && n.hasOwnProperty("label") && n.hasOwnProperty("text")))
+        throw "Not every node has properties id, successors, layer, identifiable, label, and text.";
 
       // Rule 2
       if(typeof(n.id) != "string" || n.id == "")
@@ -41,11 +49,11 @@ function verifyPolicyEncoding(nodes_data)
       else
         ids.add(n.id);
 
-      // Rule 6
+      // Rule 8
       if(n.layer < 0 || n.layer > 3 || ! Number.isInteger(n.layer))
         throw "Not every node has 0, 1, 2, or 3 as the value for layer.";
 
-      // Rule 7
+      // Rule 9
       if(n.layer == 0)
       {
         if(layer_0_found)
@@ -54,20 +62,20 @@ function verifyPolicyEncoding(nodes_data)
           layer_0_found = true;
       }
 
-      // Rule 8
+      // Rule 10
       if(typeof(n.identifiable) != "boolean")
         throw "Not every node has a boolean value for identifiable.";
 
-      // Rule 9
+      // Rule 11
       if(typeof(n.label) != "string" || n.label == "")
         throw "Not every node has a non-empty string value for label.";
 
-      // Rule 10
+      // Rule 12
       if(typeof(n.text) != "string")
         throw "Not every node has a string value for text.";
     }
 
-    // Rule 7
+    // Rule 9
     if(! layer_0_found)
       throw "No node has 0 as the value for layer.";
 
@@ -76,25 +84,29 @@ function verifyPolicyEncoding(nodes_data)
       n = nodes_data[i];
 
       // Rule 4
-      var edges_out = n.edges_out;
-      if(! Array.isArray(edges_out))
-        throw "Not every node has a string array for edges_out.";
+      var successors = n.successors;
+      if(! Array.isArray(successors))
+        throw "Not every node has a string array for successors.";
 
-      for(var j = 0; j < edges_out.length; j++)
+      for(var j = 0; j < successors.length; j++)
       {
-        var e = edges_out[j];
+        var s = successors[j];
 
         // Rule 4
-        if(typeof(e) != "string")
-          throw "Not every node has a string array for edges_out.";
-        if(! ids.has(e))
-          throw "Not every node has a string array for edges_out, where all array elements are ids of other nodes.";
+        if(typeof(s) != "string")
+          throw "Not every node has a string array for successors.";
+        if(! ids.has(s))
+          throw "Not every node has a string array for successors, where all array elements are ids of other nodes.";
 
         // Rule 5
-        if(parent_found.has(e))
-          throw "At least one node is in the edges_out array of more than one node (i.e., not every node has at most one parent).";
+        if(parent_found.has(s))
+          throw "At least one node is in the successors array of more than one node (i.e., not every node has at most one parent).";
         else
-          parent_found.add(e);
+          parent_found.add(s);
+
+        // Rule 6
+        if(n.layer > getNodeById(s, nodes_data).layer)
+          throw "Not every node has a value for layer less than or equal to those of its successors (i.e., at least one link travels inwards).";
       }
     }
 
